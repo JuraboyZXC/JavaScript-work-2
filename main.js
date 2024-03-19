f55f20f9f58a4d4ef927375dde89108ae302da3d
Maestro <gameacounto726@gmail.com>
2024-03-19 22:23:09 +0500

    js work

---
+++ main.js
@@ -0,0 +1,26 @@
+// Баланс
+
+let balance = 25000;
+
+// Траты
+let car = 4480;
+let food = 890;
+
+// Доходы
+let invoice = 5500;
+let stock = 4200;
+
+console.log(car + food);
+
+console.log(balance - (car + food));
+
+console.log(invoice + stock);
+
+console.log((balance - (car + food)) + (invoice + stock));
+
+// Итог
+let totalBalance;
+
+console.log(
+    totalBalance = ((balance - (car + food)) + (invoice + stock))   
+);
